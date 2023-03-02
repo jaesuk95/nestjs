485c77ca2d66ae8230070f7c2da115322f8e86f6
Jaesuk <jaesuk95@outlook.com>
2023-03-03 02:08:25 +0900

    get 요청 (param, body)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {Body, Controller, Get, Param, Req} from '@nestjs/common';
+import {AppService} from './app.service';
 
-@Controller()
+@Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  // param 과 body 를 받는 방법
+  @Get('hello/:id/:name')
+  getHello(@Req() req: Request,
+           @Body() body,
+           @Param() param: {id: string; name: string}): string {
+    console.log(req)
+
+    // body 이렇게 받을 수 있지만 @Body
+    // const body = req.body;
+    console.log(param);
+
+    console.log(param.id);
+    console.log(param.name);
+
     return this.appService.getHello();
   }
+
 }
