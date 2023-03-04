1b5c4843d9138b7f23627fae2e2af45dc32aa7a9
Jaesuk <jaesuk95@outlook.com>
2023-03-05 00:34:36 +0900

    .env 환경변수 & mongoose setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import {LoggerMiddleware} from "./common/logger/logger.middleware";
+import {MongooseModule} from "@nestjs/mongoose";
+import {ConfigModule} from "@nestjs/config";  // 환경 변수
 
 @Module({
-  imports: [CatsModule],  // 모듈을 연결시키는
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URL),
+    CatsModule
+  ],  // 모듈을 연결시키는
   controllers: [AppController],
   providers: [AppService],  // AppService 가 바로 공급자다
 })
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {HttpExceptionFilter} from "./common/exception/http-exception-filter";
+const PORT = process.env.SERVER_PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
-  await app.listen(8000);
+  await app.listen(PORT);
 }
 bootstrap();
